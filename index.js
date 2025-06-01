6f1b452a5d920c27e6213804b3627ab20b6f75bf
moktadir-mirul <moktadir.mirul@gmail.com>
2025-06-02 00:18:09 +0600

    basic server done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Job Portal server is running")
+})
+
+app.listen(port, () => {
+    console.log(`Job portal server is running on port - ${port}`)
+})
