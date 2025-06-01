2a90442c691160203c12b744529d74304b923058
moktadir-mirul <moktadir.mirul@gmail.com>
2025-06-02 00:36:05 +0600

    connected to mongo db

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mnwmrsu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run () {
+    try {
+    await client.connect();
+
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
     res.send("Job Portal server is running")
