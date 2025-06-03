d57971322289d14811fc8b52dbfe0005f379f5f3
moktadir-mirul <moktadir.mirul@gmail.com>
2025-06-04 01:05:06 +0600

    dynamic job route added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.use(cors());
@@ -27,8 +27,13 @@ async function run () {
 
     app.get("/jobs", async(req, res) => {
       const result = await jobsCollection.find().toArray();
-      console.log(result)
       res.send(result)
+    })
+    app.get("/jobs/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
     })
     
     // Send a ping to confirm a successful connection
