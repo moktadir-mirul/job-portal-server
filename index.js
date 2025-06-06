8622ed99559fbe4c016bf12796a2d0dda6cc5fe0
moktadir-mirul <moktadir.mirul@gmail.com>
2025-06-06 08:55:17 +0600

    job post method done

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ async function run () {
       const result = await jobsCollection.findOne(query);
       res.send(result);
     })
+
+    app.post("/jobs", async(req, res) => {
+      const newJob = req.body;
+      const result = await jobsCollection.insertOne(newJob);
+      console.log(result);
+      res.send(result)
+    })
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
